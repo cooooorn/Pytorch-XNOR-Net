588d6ff1da7e9ed47c4ff66377278a4a36e08a8a
cooooorn <rdmbuaload@gmail.com>
2018-05-13 01:59:27 +0800

    delete and update
    delete .pth file and update build.py for pytorch c-extension

--- csrc/binop/build.py
+++ csrc/binop/build.py
@@ -4,22 +4,30 @@ from os import path as osp
 from torch.utils.ffi import create_extension
 
 abs_path = osp.dirname(osp.realpath(__file__))
-extra_objects = [osp.join(abs_path, 'build/binop_cuda_kernel.so')]
-extra_objects += glob.glob('/usr/local/cuda/lib64/*.a')
+extra_objects = []
+sources = ['src/binop.c']
+headers = ['include/binop.h']
+defines = []
+with_cuda = False
 
+if torch.cuda.is_available():
+    extra_objects += [osp.join(abs_path, 'build/binop_cuda_kernel.so')]
+    extra_objects += glob.glob('/usr/local/cuda/lib64/*.a')
+    sources += ['src/binop_cuda.c']
+    headers += ['include/binop_cuda.h']
+    defines += [('WITH_CUDA', None)]
+    with_cuda = True
 ffi = create_extension(
     'binop',
-    headers=['include/binop_cuda.h']+['include/binop.h'],
-    sources=['src/binop_cuda.c']+['src/binop.c'],
-    define_macros=[('WITH_CUDA', None)],
+    headers=headers,
+    sources=sources,
+    define_macros=defines,
     relative_to=__file__,
-    with_cuda=True,
+    with_cuda=with_cuda,
     extra_objects=extra_objects,
     include_dirs=[osp.join(abs_path, 'include')],
     extra_compile_args=["-std=c99", "-O3", "-fopenmp", "-mtune=native", "-march=x86-64"]
 )
 
-
 if __name__ == '__main__':
-    assert torch.cuda.is_available(), 'Please install CUDA for GPU support.'
     ffi.build()
